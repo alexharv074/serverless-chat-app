63adcba55d8595fdc4353ef9185aeaf6b196c68a
Alex Harvey <alexharv074@gmail.com>
2019-01-07 22:08:29 +1100

    Add sam.sh script

---
+++ sam.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+. data.sh
+
+cd sam-app
+
+set -x
+
+sam build || exit $?
+
+sam package \
+  --template-file template.yaml \
+  --output-template-file packaged.yaml \
+  --s3-bucket $s3_bucket || exit $?
+
+sam deploy \
+  --template-file packaged.yaml \
+  --stack-name sam-app \
+  --capabilities CAPABILITY_IAM || exit $?
