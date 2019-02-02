1488f91aa45ddaa09eebb8171723dc2f62391d66
Alex Harvey <alexharv074@gmail.com>
2019-02-02 21:34:57 +1100

    Clean up in get-apis.sh

--- get-apis.sh
+++ get-apis.sh
@@ -3,19 +3,32 @@
 echo "APIs:"
 aws apigateway get-rest-apis | tee rest_apis.json
 
-api_id=$(jq -r '.items[].id' rest_apis.json)
+rest_api_ids=$(jq -r '.items[].id' \
+  rest_apis.json 2> /dev/null || true)
 
-echo "API resources:"
-aws apigateway get-resources --rest-api-id $api_id | tee resources.json
+for rest_api_id in $rest_api_ids ; do
 
-resource_ids=$(jq -r '.items[].id' resources.json)
-for resource_id in $resource_ids ; do
-  methods=$(jq -r --arg id $resource_id '.items[] | select(.id == $id) |
-    .resourceMethods | keys | @tsv' resources.json 2> /dev/null || true)
-  for method in $methods ; do
+  echo "API resources:"
+  aws apigateway get-resources \
+    --rest-api-id $rest_api_id | tee resources.json
 
-    echo "API method $method:"
-    aws apigateway get-method --http-method $method \
-      --resource-id $resource_id --rest-api-id $api_id | tee methods-$resource_id-$method.json
+  resource_ids=$(jq -r '.items[].id' resources.json)
+
+  for resource_id in $resource_ids ; do
+
+    methods=$(jq -r --arg id $resource_id \
+      '.items[] | select(.id == $id) | .resourceMethods | keys | @tsv' \
+        resources.json 2> /dev/null || true)
+
+    for method in $methods ; do
+
+      echo "API method $method:"
+      aws apigateway get-method --http-method $method \
+        --resource-id $resource_id --rest-api-id $rest_api_id | \
+          tee methods-$resource_id-$method.json
+
+    done
   done
 done
+
+rm -f rest_apis.json resources.json methods*json
