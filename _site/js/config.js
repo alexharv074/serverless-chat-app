7bf41a0544caca2186e586267db2784e16284b9a
Alex Harvey <alexharv074@gmail.com>
2019-02-16 20:45:59 +1100

    Correction to allow origin headers in _site/js/config.js

--- _site/js/config.js
+++ _site/js/config.js
@@ -1,3 +1,3 @@
 var ChatApp = window.ChatApp || {};
 
-ChatApp.apiEndpoint = 'https://kofqhtz5m3.execute-api.ap-southeast-2.amazonaws.com/Prod/conversations/';
+ChatApp.apiEndpoint = 'https://6ov53db113.execute-api.ap-southeast-2.amazonaws.com/Prod/';
